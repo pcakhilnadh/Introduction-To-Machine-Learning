ed8d390fd4667e556e36a484053af5797a7c44a9
pcakhilnadh <pcakhilnadh@gmail.com>
2024-04-23 22:19:37 +0530

    Linear Regression

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import math
+
+dataset = pd.read_csv("datasets/house_prices.csv")
+size=dataset['sqft_living']
+price=dataset['price']
+print(price)
